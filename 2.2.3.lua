3af6194b564f5190fe332ffe2111456f44b41413
root <root@ip-10-29-93-229.ec2.internal>
2025-08-19 21:05:37 +0000

    add grads lua file.

---
+++ 2.2.3.lua
@@ -0,0 +1,19 @@
+whatis("Name : grads")
+whatis("Version : 2.2.1")
+whatis("Target : x86_64_v4")
+whatis("Short description : The Grid Analysis and Display System (GrADS) is an interactive desktop tool that is used for easy access, manipulation, and visualization of earth science data. GrADS has two data models for handling gridded and station data. GrADS supports many data file formats, including binary (stream or sequential), GRIB (version 1 and 2), NetCDF, HDF (version 4 and 5), and BUFR (for station data).")
+whatis("Configure options : --with-geotiff --with-hdf4 --with-hdf5 --with-netcdf --without-gadap")
+help([[Name   : grads]])
+help([[Version: 2.2.3]])
+help([[Target : x86_64_v4]])
+help()
+help([[The Grid Analysis and Display System (GrADS) is an interactive desktop
+tool that is used for easy access, manipulation, and visualization of
+earth science data. GrADS has two data models for handling gridded and
+station data. GrADS supports many data file formats, including binary
+(stream or sequential), GRIB (version 1 and 2), NetCDF, HDF (version 4
+and 5), and BUFR (for station data).]])
+prepend_path("PATH","/opt/spack/opt/spack/linux-sapphirerapids/grads-2.2.3-yc7lor7wsubr5uszv2v5evbvdw273md5/bin")
+prepend_path("LD_LIBRARY_PATH","/opt/spack/opt/spack/linux-sapphirerapids/grads-2.2.3-yc7lor7wsubr5uszv2v5evbvdw273md5/lib")
+setenv("GADDIR","/opt/spack/opt/spack/linux-sapphirerapids/grads-2.2.3-yc7lor7wsubr5uszv2v5evbvdw273md5/data")
+setenv("GRADS_ROOT","/opt/spack/opt/spack/linux-sapphirerapids/grads-2.2.3-yc7lor7wsubr5uszv2v5evbvdw273md5")
